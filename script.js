6310b2b93b6cf0d0451bf476d442095e4a6f2b3b
starrymeadows <aliharris.star@gmail.com>
2022-09-20 23:14:24 -0400

    Add most functionality

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let result;
+let calcNum = [];
+let currentNum = null;
+let calculator = {
+    a: null,
+    b: null,
+    operator: null,
+};
 
+// calculator basic logic
 function add(a, b) {
     return a + b;
 }
@@ -13,20 +20,102 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b == 0) return "Nope."
     return (a / b);
 }
 
 function operate(operator, a, b) {
     switch (operator) {
-        case "add":
-            return add(a, b);
-        case "subtract":
-            return subtract(a, b);
-        case "multiply":
-            return multiply(a, b);
-        case "divide":
-            return divide(a, b);
+        case "+":
+            currentNum = add(a, b);
+            break;
+        case "-":
+            currentNum = subtract(a, b);
+            break;
+        case "x":
+            currentNum = multiply(a, b);
+            break;
+        case "/":
+            currentNum = divide(a, b);
+            break;
         default:
-            return "Oops!";
+            currentNum = "Oops!";
     }
+}
+
+// function to append clicked number (store in a, then store in b)
+// store as array
+
+// function to select operator
+
+// populate display
+
+const numBtns = document.querySelectorAll('.number');
+const operBtns = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+
+const display = document.querySelector('.display');
+
+
+numBtns.forEach(button =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+    );
+
+operBtns.forEach(button =>
+    button.addEventListener('click', () => handleOperator(button.textContent))
+    );
+
+clearBtn.addEventListener('click', () => {
+    calculator.a = null;
+    calculator.b = null;
+    calculator.operator = null;
+    calcNum = [];
+    currentNum = 0;
+    updateDisplay();
+});
+
+deleteBtn.addEventListener('click', () => {
+    if (calcNum.length === 0) calcNum = currentNum.toString().split("");
+    calcNum.pop();
+    convertNum();
+    updateDisplay();
+})
+
+function appendNumber(num) {
+    calcNum.push(num);
+    convertNum();
+    updateDisplay();
+}
+
+function convertNum() {
+    currentNum = calcNum.toString().replace(/[^\d\.]/g, "");
+    if (currentNum.length > 11) currentNum = currentNum.toFixed(11);
+    currentNum = +currentNum;
+}
+
+function updateDisplay() { 
+    display.textContent = currentNum;
+}
+
+function handleOperator(operator) {
+    if (calculator.a === null) {
+        calculator.a = currentNum;
+        calculator.operator = operator;
+        calcNum = [];
+    } else if (calculator.operator == "=") {
+        calculator.operator = operator;
+    } else {
+        calculator.b = currentNum;
+        evaluate();
+        calculator.operator = operator;
+    };
+}
+
+function evaluate() {
+    operate(calculator.operator, calculator.a, calculator.b);
+    updateDisplay();
+    calculator.a = currentNum;
+    calculator.b = null;
+    calcNum = [];
 }
